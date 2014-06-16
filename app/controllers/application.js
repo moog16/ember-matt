42bdbb9f9ba8b1ba71258c1846e6833fa63ac77b
mgoo <mgoo@switchfly.com>
2014-06-16 10:37:33 -0700

    WIP - full page scrolling

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -7,5 +7,31 @@ export default Ember.Controller.extend({
       utils.resizeBackgroundHeight();
       $(window).resize(utils.resizeBackgroundHeight);
     });
+
+    $(window).bind('mousewheel DOMMouseScroll', function(e) {
+      var $background = $('.background-splash.fold'),
+          bTop = parseInt($background.css('top')),
+          delta = e.originalEvent.wheelDeltaY,
+          windowHeight = $(window).height();
+
+      var setNewHeight = function(top, d) {
+        d = d || 0;
+        var newBTop = top + d;
+        console.log(newBTop);
+        $background.css('top', newBTop);
+      };
+      if(bTop >= 0 && delta > 0) {
+        setNewHeight(0);
+        return;
+      }
+      else if(Math.abs(windowHeight) < Math.abs(bTop)) {
+        if(delta > 0) {
+          setNewHeight(bTop, delta);
+        }
+        return;
+      }
+
+      setNewHeight(bTop, delta);
+    });
   }
 });
