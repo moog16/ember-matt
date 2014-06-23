7921f10821bdfbe322ba2e5e4eda0903f1fecb6d
mgoo <mgoo@switchfly.com>
2014-06-23 12:46:01 -1000

    removed weird parallax effect

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -8,26 +8,5 @@ export default Ember.Controller.extend({
       utils.resizeBackgroundHeight();
       $(window).resize(utils.resizeBackgroundHeight);
     });
-
-    $(window).bind('mousewheel DOMMouseScroll', function(e) {
-      var $background = $('.background-splash.fold'),
-          bTop = parseInt($background.css('top')),
-          delta = e.originalEvent.wheelDeltaY,
-          windowHeight = $(window).height();
-
-      if(bTop >= 0 && delta > 0) {
-        // don't allow background to go further than the top
-        utils.setNewHeight(0, 0, $background);
-      } else if(Math.abs(windowHeight) < Math.abs(bTop)) {
-        if(delta > 0) {
-          utils.setNewHeight(bTop, delta, $background);
-        }
-        // if the bkgrnd is already off page, stop moving it
-        return;
-      } else {
-        // move it if in range
-        utils.setNewHeight(bTop, delta, $background);
-      }
-    });
   }
 });
